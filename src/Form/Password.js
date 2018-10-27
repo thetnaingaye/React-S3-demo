c8a82094b91b19535e73c03c03af365df1badae1
Thet Naing Aye <mmu1tn@gmail.com>
2018-10-28 00:38:53 +0800

    password.js modified

--- src/Form/Password.js
+++ src/Form/Password.js
@@ -3,8 +3,9 @@ import React from 'react';
 const Password = (props) => {
     return (
         <div className="form-group">
-        <input className="form-control" type="password" placeholder="*Password" value={props.password}
-            onChange={props.passwordInputHandler}></input>
+        <input className="form-control" type="password" placeholder="*Password" 
+                value={props.password}
+                onChange={props.passwordInputHandler}></input>
         {props.passworderror &&
         <p className="form-error">{props.passworderror}</p> }
         </div>
