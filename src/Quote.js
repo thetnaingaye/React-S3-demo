4f61bd65abd0073e8bf7a3c0de640dcee2dc6a95
Thet Naing Aye <mmu1tn@gmail.com>
2018-10-28 15:16:19 +0800

    added quote component

---
+++ src/Quote.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import Header from './Header';
+import './Quote.css';
+class Quote extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            quote: []
+        }
+    }
+
+    componentDidMount() {
+        fetch('https://talaikis.com/api/quotes/')
+            .then((response) => {
+                return response.json();
+            })
+            .then((resultJson) => {
+                this.setState({
+                    quote: resultJson
+                })
+            });
+    }
+
+    render() {
+        const loading = (
+            <div className="quote"><h2 >Loading quotes...</h2> <h5> from https://talaikis.com/api/quotes/</h5></div>
+        );
+
+        const quotes = (
+            this.state.quote.map((q) => {
+                return (
+                    <div className="quote">
+                        <blockquote>
+                        {q.quote}
+                            <br/><span className="author"><i>{q.author}</i></span>
+                        </blockquote>
+                    </div>
+                );
+            })
+        );
+
+        return (
+            <div>
+                <Header />
+                {this.state.quote.length ===0?  loading : quotes}
+            </div>
+        );
+    }
+}
+
+
+export default Quote
