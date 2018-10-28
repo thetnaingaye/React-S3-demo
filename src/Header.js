860f55efadf947a9f37512b40f72b40650dd2dc2
Thet Naing Aye <mmu1tn@gmail.com>
2018-10-28 16:20:18 +0800

    added nav bar collapse and fixed top

--- src/Header.js
+++ src/Header.js
@@ -4,19 +4,27 @@ import { NavLink } from 'react-router-dom'
 const Header = (porps) => {
     return (
         <div>
-            <nav className="navbar navbar-inverse navbar-static-top">
+            <nav className="navbar navbar-inverse navbar-fixed-top">
                 <div className="container-fluid">
                     <div className="navbar-header">
+                        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </button>
                         <NavLink className="navbar-brand" to="/">Naing React App</NavLink>
                     </div>
-                    <ul className="nav navbar-nav">
-                        <li><NavLink to="/"><span class="glyphicon glyphicon-home"></span> Home</NavLink></li>
-                        <li><NavLink to="/Quotes"><span class="glyphicon glyphicon-leaf"></span> Quotes</NavLink></li>
 
-                    </ul>
-                    <ul className="nav navbar-nav navbar-right">
-                        <li><NavLink to="/form"><span class="glyphicon glyphicon-user"></span> Register</NavLink></li>
-                    </ul>
+                    <div class="collapse navbar-collapse" id="myNavbar">
+                        <ul className="nav navbar-nav">
+                            <li><NavLink to="/"><span class="glyphicon glyphicon-home"></span> Home</NavLink></li>
+                            <li><NavLink to="/Quotes"><span class="glyphicon glyphicon-leaf"></span> Quotes</NavLink></li>
+
+                        </ul>
+                        <ul className="nav navbar-nav navbar-right">
+                            <li><NavLink to="/form"><span class="glyphicon glyphicon-user"></span> Register</NavLink></li>
+                        </ul>
+                    </div>
                 </div>
             </nav>
         </div>
